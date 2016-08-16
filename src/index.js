5a80f4cb4b9e55177b11be3c9fb166216f62e2bf
Omri Gilad <omri.gilad@gmail.com>
2016-08-16 11:59:28 +0300

    v1.0.0

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import jsdom from 'jsdom';
+
+module.exports = function(source) {
+  this.cacheable();
+  const cb = this.async();
+  jsdom.env(source, (err, window) => {
+    if (err) {
+      return cb(err);
+    }
+
+    const result = JSON.stringify(Array.from(window.document.getElementsByTagName('path'))
+      .map((element) => element.getAttribute('d')));
+
+    console.log(result);
+
+    cb(null, `module.exports = ${result};`);
+  })
+}
